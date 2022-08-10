1d80d5f5430e6a8057b5c1121867a43c9db5dca0
Carl Welch <carlwelchdesign@gmail.com>
2022-08-09 21:16:21 -0700

    clean up

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -25,143 +25,141 @@ export const rowsInputDefault: TargetTableTypes = {
   memory: 0
 }
 
-export const getAccordianForms = ({setBenchmarkChecked, setAccelerateChecked, benchmarkChecked, accelerateChecked, register, watch}: AccordianDataProps) => {
+export const getAccordianForms = ({ setBenchmarkChecked, setAccelerateChecked, benchmarkChecked, accelerateChecked, register, watch }: AccordianDataProps) => {
   return [
-  {
-    title: 'Benchmark',
-    detail: 'Benchmarks are designed to measure the performance and accuracy of embedded inference. This allows organizations to develop plans on making improvements or adapting specific best practices, usually to increase some aspect of performance.',
-    handleCheckBoxChange: () => {
-      setBenchmarkChecked(!benchmarkChecked)
+    {
+      title: 'Benchmark',
+      detail: 'Benchmarks are designed to measure the performance and accuracy of embedded inference. This allows organizations to develop plans on making improvements or adapting specific best practices, usually to increase some aspect of performance.',
+      handleCheckBoxChange: () => {
+        setBenchmarkChecked(!benchmarkChecked)
+      },
+      checked: benchmarkChecked,
+      optionsTable: (
+        <TableContainer component={Paper}>
+          <Table aria-label="caption table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ paddingBottom: '10px' }} width="35%"><TableHeadText>ENGINE</TableHeadText></TableCell>
+                <TableCell sx={{ paddingBottom: '10px' }} width="35%"><TableHeadText>HARDWARE</TableHeadText></TableCell>
+                <TableCell sx={{ paddingBottom: '10px' }} width="15%"><TableHeadText>NUM TRIALS</TableHeadText></TableCell>
+                <TableCell sx={{ paddingBottom: '10px' }} width="15%"><TableHeadText>RUNS PER TRIAL</TableHeadText></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Select sx={{ minWidth: '100%' }}
+                    displayEmpty
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return 'Select Engine'
+                      }
+                      return selected
+                    }}
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    defaultValue={''}
+                    {...register('benchmarkEngine')}
+                  >
+                    {Object.keys(Engine).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
+                  </Select>
+                </TableCell>
+                <TableCell>
+                  <Select sx={{ minWidth: '100%' }}
+                    displayEmpty
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return 'Select Hardware'
+                      }
+                      return selected
+                    }}
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    disabled={!watch('benchmarkEngine')}
+                    defaultValue={''}
+                    {...register('benchmarkHardware')}
+                  >
+                    {Object.keys(Providers).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
+                  </Select>
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    disabled={!watch('benchmarkHardware')}
+                    InputProps={{ inputProps: { min: 1, max: 16 } }}
+                    {...register('num_trials')}
+                    defaultValue={1}
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    disabled={!watch('benchmarkHardware')}
+                    InputProps={{ inputProps: { min: 1, max: 16 } }}
+                    {...register('runs_per_trial')}
+                    defaultValue={1}
+                  />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )
     },
-    checked: benchmarkChecked,
-    optionsTable: (
-      <TableContainer component={Paper}>
-        <Table aria-label="caption table">
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ paddingBottom: '10px' }} width="35%"><TableHeadText>ENGINE</TableHeadText></TableCell>
-              <TableCell sx={{ paddingBottom: '10px' }} width="35%"><TableHeadText>HARDWARE</TableHeadText></TableCell>
-              <TableCell sx={{ paddingBottom: '10px' }} width="15%"><TableHeadText>NUM TRIALS</TableHeadText></TableCell>
-              <TableCell sx={{ paddingBottom: '10px' }} width="15%"><TableHeadText>RUNS PER TRIAL</TableHeadText></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <Select sx={{ minWidth: '100%' }}
-                  displayEmpty
-                  renderValue={(selected) => {
-                    if (selected.length === 0) {
-                      return 'Select Engine'
-                    }
-                    return selected
-                  }}
-                  inputProps={{ 'aria-label': 'Without label' }}
-                  defaultValue={''}
-                  {...register('benchmarkEngine')}
-                >
-                  {Object.keys(Engine).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
-                </Select>
-              </TableCell>
-              <TableCell>
-                <Select sx={{ minWidth: '100%' }}
-                  displayEmpty
-                  renderValue={(selected) => {
-                    if (selected.length === 0) {
-                      return 'Select Hardware'
-                    }
-                    return selected
-                  }}
-                  inputProps={{ 'aria-label': 'Without label' }}
-                  disabled={!watch('benchmarkEngine')}
-                  defaultValue={''}
-                  {...register('benchmarkHardware')}
-                >
-                  {Object.keys(Providers).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
-                </Select>
-              </TableCell>
-              <TableCell>
-                <TextField
-                  type="number"
-                  disabled={!watch('benchmarkHardware')}
-                  InputProps={{ inputProps: { min: 1, max: 16 } }}
-                  {...register('num_trials')}
-                  defaultValue={1}
-                />
-              </TableCell>
-              <TableCell>
-                <TextField
-                  type="number"
-                  disabled={!watch('benchmarkHardware')}
-                  InputProps={{ inputProps: { min: 1, max: 16 } }}
-                  {...register('runs_per_trial')}
-                  defaultValue={1}
-                />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-)
-
-
-  },
-  {
-    title: 'Accelerate',
-    detail: 'An AI accelerator is a high-performance parallel computation machine that is specifically designed for the efficient processing of AI workloads like neural networks.',
-    handleCheckBoxChange: () => {
-      setAccelerateChecked(!accelerateChecked)
+    {
+      title: 'Accelerate',
+      detail: 'An AI accelerator is a high-performance parallel computation machine that is specifically designed for the efficient processing of AI workloads like neural networks.',
+      handleCheckBoxChange: () => {
+        setAccelerateChecked(!accelerateChecked)
+      },
+      checked: accelerateChecked,
+      optionsTable: (
+        <TableContainer component={Paper}>
+          <Table aria-label="caption table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ paddingBottom: '10px' }} width="50%"><TableHeadText>ENGINE</TableHeadText></TableCell>
+                <TableCell sx={{ paddingBottom: '10px' }} width="50%"><TableHeadText>HARDWARE</TableHeadText></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Select sx={{ minWidth: '100%' }}
+                    displayEmpty
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return 'Select Engine'
+                      }
+                      return selected
+                    }}
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    defaultValue={''}
+                    {...register('accelerateEngine')}
+                  >
+                    {Object.keys(Engine).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
+                  </Select>
+                </TableCell>
+                <TableCell>
+                  <Select sx={{ minWidth: '100%' }}
+                    displayEmpty
+                    renderValue={(selected) => {
+                      if (selected.length === 0) {
+                        return 'Select Hardware'
+                      }
+                      return selected
+                    }}
+                    disabled={!watch('accelerateEngine')}
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    defaultValue={''}
+                    {...register('accelerateHardware')}
+                  >
+                    {Object.keys(Providers).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
+                  </Select>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ),
     },
-    checked: accelerateChecked,
-    optionsTable: (
-      <TableContainer component={Paper}>
-        <Table aria-label="caption table">
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ paddingBottom: '10px' }} width="50%"><TableHeadText>ENGINE</TableHeadText></TableCell>
-              <TableCell sx={{ paddingBottom: '10px' }} width="50%"><TableHeadText>HARDWARE</TableHeadText></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <Select sx={{ minWidth: '100%' }}
-                  displayEmpty
-                  renderValue={(selected) => {
-                    if (selected.length === 0) {
-                      return 'Select Engine'
-                    }
-                    return selected
-                  }}
-                  inputProps={{ 'aria-label': 'Without label' }}
-                  defaultValue={''}
-                  {...register('accelerateEngine')}
-                >
-                  {Object.keys(Engine).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
-                </Select>
-              </TableCell>
-              <TableCell>
-                <Select sx={{ minWidth: '100%' }}
-                  displayEmpty
-                  renderValue={(selected) => {
-                    if (selected.length === 0) {
-                      return 'Select Hardware'
-                    }
-                    return selected
-                  }}
-                  disabled={!watch('accelerateEngine')}
-                  inputProps={{ 'aria-label': 'Without label' }}
-                  defaultValue={''}
-                  {...register('accelerateHardware')}
-                >
-                  {Object.keys(Providers).map(key => <MenuItem key={key} value={key}>{key}</MenuItem>)}
-                </Select>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    ),
-  },
-]
+  ]
 }
