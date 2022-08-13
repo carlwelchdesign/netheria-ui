3638c47be7652e48b7bee6a32d903038bd2986b2
Carl Welch <carlwelchdesign@gmail.com>
2022-08-13 13:54:32 -0700

    cleaned up

--- src/components/MainGrid/index.tsx
+++ src/components/MainGrid/index.tsx
@@ -26,44 +26,42 @@ const MainGrid = ({
   duplicateAlert,
   targetTable,
   octomizePanel,
-}: Props) => {
-  return (
-    <Box sx={flexStyle}>
-      <SideNav />
-      <Container>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            {modelTitleDetails}
-          </Grid>
-          <Grid item xs={9}>
-            <Paper sx={mainPanelStyle} elevation={6}>
-              <Box sx={accorianContainerStyle}>
-                <Typography sx={octomizeTitleTextStyle} variant='h3'>
-                  Octomize
-                </Typography>
-                {accordianForm}
-                <Grid container spacing={0}>
-                  <Grid item xs={11} sx={hardwareTargetTableHeadingStyle}>
-                    <Typography sx={hardwareTargetTextStyle} variant='body1'>
-                      Hardware targets
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={1} sx={flexStyle}>
-                    {addRowButton}
-                  </Grid>
-                </Grid>
-              </Box>
-              {duplicateAlert}
-              {targetTable}
-            </Paper>
-          </Grid>
-          <Grid item xs={3}>
-            {octomizePanel}
-          </Grid>
+}: Props) => (
+  <Box sx={flexStyle}>
+    <SideNav />
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          {modelTitleDetails}
         </Grid>
-      </Container>
-    </Box>
-  )
-}
+        <Grid item xs={9}>
+          <Paper sx={mainPanelStyle} elevation={6}>
+            <Box sx={accorianContainerStyle}>
+              <Typography sx={octomizeTitleTextStyle} variant='h3'>
+                Octomize
+              </Typography>
+              {accordianForm}
+              <Grid container spacing={0}>
+                <Grid item xs={11} sx={hardwareTargetTableHeadingStyle}>
+                  <Typography sx={hardwareTargetTextStyle} variant='body1'>
+                    Hardware targets
+                  </Typography>
+                </Grid>
+                <Grid item xs={1} sx={flexStyle}>
+                  {addRowButton}
+                </Grid>
+              </Grid>
+            </Box>
+            {duplicateAlert}
+            {targetTable}
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          {octomizePanel}
+        </Grid>
+      </Grid>
+    </Container>
+  </Box>
+)
 
 export default MainGrid
